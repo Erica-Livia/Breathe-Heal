9a8e1454ce633e81a0fe9706448397999f89b6ea
Erica-Livia <e.ingabire1@alustudent.com>
2024-03-09 14:18:50 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(Homepage());
+  runApp(Welcome());
 }
 
 class MyApp extends StatelessWidget {
